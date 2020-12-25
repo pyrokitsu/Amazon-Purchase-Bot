67431dc91c8221e4e881cd3b1d4154afd0627a09
pyrokitsu <paironya@gmail.com>
2020-12-25 18:36:34 +0100

    Migrating add to cart method to buy now method
    fuck announce

--- amazon-bot.py
+++ amazon-bot.py
@@ -10,7 +10,7 @@ class AmazonBot:
     def main(self):
         self.driver.get('https://www.amazon.fr')
         # Default = 15, change this setting depending on your connection.
-        sleep(15)
+        sleep(10)
         accept_cookie = self.driver.find_element_by_xpath('//*[@id="sp-cc-accept"]')
         accept_cookie.click()
         readme_disclaimer = input(
@@ -32,28 +32,23 @@ class AmazonBot:
     def check_and_buy(self):
         try:
             # Change the link of the product that you want to buy!
-            self.driver.get("AMAZON LINK HERE")
-            buy_now = self.driver.find_element_by_xpath('//*[@id="add-to-cart-button"]')
+            self.driver.get("LINK HERE")
+            buy_now = self.driver.find_element_by_xpath('//*[@id="buy-now-button"]')
             buy_now.click()
-            sleep(2)
-            buy_now_announce = self.driver.find_element_by_xpath('//*[@id="siNoCoverage-announce"]')
-            buy_now_announce.click()
             sleep(2)
         except Exception as e:
             if e:
-                print("Cannot find the {Add to Cart} button! Refreshing now...")
+                print("Cannot find the {Buy Now} button! Refreshing now...")
                 sleep(1.5)
                 clear_screen()
                 self.check_and_buy()
         finally:
-            buy_now2 = self.driver.find_element_by_xpath('//*[@id="hlb-ptc-btn-native"]')
-            buy_now2.click()
-            sleep(2)
-            buy_now3 = self.driver.find_element_by_xpath(
-                '/html/body/div[5]/div/div[2]/form/div/div/div/div[2]/div/div[1]/div/div[1]/div/span/span/input')
-            buy_now3.click()
-            sleep(60)
-            self.driver.close()
+            if self.driver.find_element_by_xpath('//*[@id="placeYourOrder"]'):
+                self.driver.find_element_by_xpath('//*[@id="placeYourOrder"]').click()
+            elif self.driver.find_element_by_xpath('//*[@id="turbo-checkout-pyo-button"]'):    
+                self.driver.find_element_by_xpath('//*[@id="turbo-checkout-pyo-button"]').click()
+        sleep(120)
+        self.driver.close()
 
 
 sys_windows = os.name == "nt"
