2d08925b57bfe556d4ae776f4c5abaeb2ddeb874
pyrokitsu <pyrokitsu1@gmail.com>
2021-02-14 22:11:12 +0100

    + adding some fixes and improve the code

--- amazon-bot.py
+++ amazon-bot.py
@@ -22,6 +22,11 @@ class AmazonBot:
             os.system('Taskkill.exe /IM cmd.exe')
         elif readme_disclaimer == 'y':
             clear_screen()
+            self.driver.get('https://www.amazon.fr/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F'
+                            '%2Fwww.amazon.fr%2Fref%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth'
+                            '%2F2.0%2Fidentifier_select&openid.assoc_handle=frflex&openid.mode=checkid_setup&openid'
+                            '.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns'
+                            '=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
             print("Please login to your Amazon account! (You have 60 seconds!)")
             # Default = 60, change this setting depending on your speed to login at your account.
             sleep(60)
@@ -33,20 +38,21 @@ class AmazonBot:
         try:
             # Change the link of the product that you want to buy!
             self.driver.get("LINK HERE")
-            buy_now = self.driver.find_element_by_xpath('//*[@id="buy-now-button"]')
-            buy_now.click()
             sleep(2)
+            add = self.driver.find_element_by_xpath('//*[@id="add-to-cart-button"]')
+            add.click()
+            sleep(2)
+            buy = self.driver.find_element_by_xpath('//*[@id="attach-sidesheet-checkout-button"]')
+            buy.click()
+            sleep(2)
+            pay = self.driver.find_element_by_xpath('//*[@id="placeYourOrder"]')
+            pay.click()
         except Exception as e:
             if e:
-                print("Cannot find the {Buy Now} button! Refreshing now...")
+                print("Cannot find the {add to cart} button! Refreshing now...")
                 sleep(1.5)
                 clear_screen()
                 self.check_and_buy()
-        finally:
-            if self.driver.find_element_by_xpath('//*[@id="placeYourOrder"]'):
-                self.driver.find_element_by_xpath('//*[@id="placeYourOrder"]').click()
-            elif self.driver.find_element_by_xpath('//*[@id="turbo-checkout-pyo-button"]'):    
-                self.driver.find_element_by_xpath('//*[@id="turbo-checkout-pyo-button"]').click()
         sleep(120)
         self.driver.close()
 
